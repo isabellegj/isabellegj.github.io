cc9ab313db4c72403d916c19fc0a9c5f905bee84
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-05 20:31:35 -0300

    isolated block in a function

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+// Smooth Scroll
 
-function scrollToSection(event) {
-  event.preventDefault();
+function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
 
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
+  function scrollToSection(event) {
+    event.preventDefault();
 
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
 
-internalLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
+initSmoothScroll();
