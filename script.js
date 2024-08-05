0cf4059f17abbc6435a5bf2a3b1ee58c33ffe271
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-05 20:28:25 -0300

    added script file and implemented smooth scroll

---
+++ script.js
@@ -0,0 +1,17 @@
+const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+internalLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
