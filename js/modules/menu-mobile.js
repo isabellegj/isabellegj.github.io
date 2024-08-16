b1b91e39145afd252eeee4f0dc5c90ffa38910fd
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-08-16 18:48:49 -0300

    test sign

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -5,6 +5,8 @@ export default function initMenuMobile() {
   const menuList = document.querySelector(".menu");
   const eventos = ["click", "touchend"];
 
+  // teste
+
   if (menuButton) {
     function openMenu(event) {
       menuList.classList.add("active");
